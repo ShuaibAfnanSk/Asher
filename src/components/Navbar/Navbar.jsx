616fa220f8b7f0ccfd74b04e9fa2dd4091d7b948
Sai Chandu K <91937496+Chandu-5@users.noreply.github.com>
2025-01-31 21:18:17 +0530

    added responsive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { IoMdMenu } from "react-icons/io";
+import { FaTimes } from 'react-icons/fa';
 import Logo from '../../assets/images/logo.png';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className='flex fixed w-full justify-between p-4 bg-teal-950 text-white rounded-b-md z-50'>
+    <div className='flex fixed w-full justify-between p-4 bg-teal-950 text-white rounded-b-md z-50 transition-colors duration-300'>
       {/* logo */}
       <div className="flex py-2 px-4">
         <h1 className='text-2xl font-semibold logo'>Asher</h1>
@@ -14,13 +17,13 @@ function Navbar() {
 
       {/* NavItems */}
       <div className="flex items-center py-2 px-4">
-        <div className="flex md:hidden">
-          <IoMdMenu size={25} />
+        <div className="flex md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FaTimes size={25} /> : <IoMdMenu size={25} />}
         </div>
-        <nav className='hidden md:flex space-x-8 font-medium text-[20px]'>
-          <Link onClick={() => window.scrollTo(0, 0)} to={'/'}>Home</Link>
-          <Link onClick={() => window.scrollTo(0, 0)} to={'/about'}>About</Link>
-          <Link onClick={() => window.scrollTo(0, 0)} to={'/contact'}>Contact</Link>
+        <nav className={`fixed flex flex-col items-center justify-start pt-28 md:pt-0 space-y-8 md:flex-row top-0 left-0 w-3/4 h-full bg-teal-950 font-medium text-[20px] transition-transform transform ${menuOpen ? 'translate-x-0' : '-translate-x-full'} md:relative md:flex-row md:space-y-0 md:space-x-8 md:text-[20px] md:translate-x-0`}>
+          <Link onClick={() => { window.scrollTo(0, 0); setMenuOpen(false); }} to={'/'}>Home</Link>
+          <Link onClick={() => { window.scrollTo(0, 0); setMenuOpen(false); }} to={'/about'}>About</Link>
+          <Link onClick={() => { window.scrollTo(0, 0); setMenuOpen(false); }} to={'/contact'}>Contact</Link>
         </nav>
       </div>
     </div>
