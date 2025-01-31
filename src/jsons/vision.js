b41dd1dad899af032a642db4b55fb0d8c137c2f8
Sai Chandu K <91937496+Chandu-5@users.noreply.github.com>
2025-01-31 23:04:43 +0530

    fix deployment error

--- src/jsons/vision.js
+++ src/jsons/vision.js
@@ -1,5 +1,5 @@
 import mission from "../assets/mission.png";
-import vision from "../assets/vision.png";
+import visionimg from "../assets/vision.png";
 
 const vision = [
     {
@@ -9,7 +9,7 @@ const vision = [
     },
     {
         name: "Vision",
-        image: {vision},
+        image: {visionimg},
         description: "To create a world where people honor the gifts of nature in their daily lives, choosing wellness over trends and natural solutions over artificial shortcuts. Our vision is to lead this change by offering nature’s purest luxuries, helping every individual build a stronger, healthier, and more fulfilling life."
     }
 ]
