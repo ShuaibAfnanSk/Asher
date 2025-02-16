b3e0a29d36287ac2fbbec96913b0ff6ae488379c
ShuaibAfnanSk <ivoxatomig@gmail.com>
2025-02-16 17:30:08 +0530

    changes laoder

--- src/App.jsx
+++ src/App.jsx
@@ -52,9 +52,12 @@ function App() {
       {/* Loader */}
       {loading && (
         <div className={`loader-container ${animateOut ? "move-up" : ""}`}>
-          <div className="spinner">
-            <img src={orange} alt="" />
-          </div>
+          <svg class="pl" width="240" height="240" viewBox="0 0 240 240">
+            <circle class="pl__ring pl__ring--a" cx="120" cy="120" r="105" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 660" stroke-dashoffset="-330" stroke-linecap="round"></circle>
+            <circle class="pl__ring pl__ring--b" cx="120" cy="120" r="35" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 220" stroke-dashoffset="-110" stroke-linecap="round"></circle>
+            <circle class="pl__ring pl__ring--c" cx="85" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
+            <circle class="pl__ring pl__ring--d" cx="155" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
+          </svg>
         </div>
       )}
 
